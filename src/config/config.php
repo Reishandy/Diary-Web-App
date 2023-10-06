0aeabe4a905070fc75926c6a87ebba3a84028e13
Reishandy <isthisruxury@gmail.com>
2023-10-07 07:03:41 +0800

    Encryption module
    Signed-off-by: Reishandy <isthisruxury@gmail.com>

---
+++ src/config/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Config file containing constant variables for use in this project
+ *
+ * @author Reishandy (isthisruxury@gmail.com)
+ */
+
+// Security module's constants
+const ENCRYPTION_ALGORITHM = "AES-256-CBC";
+const HASH_ALGORITHM = "sha3-256";
+const PRIMARY_SIZE = 32;
+const SECONDARY_SIZE = 16;
+const ITERATION = 10000;
